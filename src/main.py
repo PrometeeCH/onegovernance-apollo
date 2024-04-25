7dba9b460897aa396b96d8c9975ed83e354f1ca9
George Abi Younes <georgeabiyounes@gmail.com>
2024-04-25 17:23:19 +0200

    Updated main.py to match the version in dev branch

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
+import json
+import os
+
 import streamlit as st
+
+from apollo.VectorStore import VectorStore
 
 
 def get_ai_response(user_input: str) -> str:
@@ -10,16 +15,58 @@ def main() -> None:
     st.set_page_config(page_title="Apollo", layout="wide")
     st.title("Apollo")
 
-    # Sidebar for document upload
-    with st.sidebar:
-        st.header("Document Upload")
-        uploaded_file = st.file_uploader("Choose a file")
-        if uploaded_file is not None:
-            st.success("File uploaded successfully!")
+    vector_store = VectorStore()
+
+    if "delete_status" not in st.session_state:
+        st.session_state.delete_status = ""
 
     # Initialize chat state
     if "messages" not in st.session_state:
         st.session_state.messages = []
+
+    # Sidebar for document upload
+    with st.sidebar:
+        st.header("Upload a Document")
+        uploaded_file = st.file_uploader("Choose a file (only PDF)", type=["pdf"])
+        if uploaded_file is not None:
+            try:
+                with open("temp_file.pdf", "wb") as f:
+                    f.write(uploaded_file.getvalue())
+
+                try:
+                    title = vector_store.extract_title(pdf_path="temp_file.pdf")
+                    vector_store.push_document(
+                        document_path="temp_file.pdf", title=title
+                    )
+                    st.success("File uploaded successfully!")
+                except Exception as e:
+                    st.error(f"Error while uploading the file. {e}")
+            except Exception as e:
+                st.error(f"Error while uploading the file. {e}")
+
+            os.remove("temp_file.pdf")
+
+        # Display documents in the index
+        st.header("Uploaded Documents")
+        for doc in vector_store.fetch_documents(max_results=20):
+            col1, col2 = st.columns([0.8, 0.2])
+            with col1:
+                metadata = json.loads(doc["metadata"])
+                st.text(metadata["title"])
+            with col2:
+                if st.button("✖", key=doc["id"]):
+                    result = vector_store.delete_document(doc["id"])
+                    if isinstance(result, str) and len(result) > 0:  # Error case
+                        st.session_state.delete_status = (
+                            f"Failed to delete document: {result}"
+                        )
+                    else:
+                        st.session_state.delete_status = (
+                            "Document deleted successfully!"
+                        )
+                        st.rerun()  # Rerun to refresh the document list
+
+    st.sidebar.text(st.session_state.delete_status)
 
     # Chat input using st.chat_input
     user_input = st.chat_input("Type your message here...", key="chat")
